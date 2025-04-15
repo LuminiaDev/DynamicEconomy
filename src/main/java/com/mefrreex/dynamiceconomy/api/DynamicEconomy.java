91f1b2dba7489476cc45ba6abb8b55ec67692c50
MEFRREEX <mefrreex@gmail.com>
2025-04-16 02:25:33 +0400

    docs: basic javadoc

--- src/main/java/com/mefrreex/dynamiceconomy/api/DynamicEconomy.java
+++ src/main/java/com/mefrreex/dynamiceconomy/api/DynamicEconomy.java
@@ -5,20 +5,61 @@ import com.mefrreex.dynamiceconomy.api.controller.price.PriceController;
 import com.mefrreex.dynamiceconomy.impl.DynamicEconomyImpl;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents the core interface for managing a dynamic economy.
+ * Provides methods to query and manipulate item counts, as well as retrieve dynamic pricing.
+ */
 public interface DynamicEconomy {
 
+    /**
+     * Gets the current price of an item by its ID.
+     *
+     * @param id The item identifier
+     * @return The current price of the item
+     */
     double getCurrentPrice(String id);
 
+    /**
+     * Increases the count of an item in the economy.
+     *
+     * @param id The item identifier
+     * @param count The amount to increase by
+     */
     void increaseItemCount(String id, int count);
 
+    /**
+     * Decreases the count of an item in the economy.
+     *
+     * @param id The item identifier
+     * @param count The amount to decrease by
+     */
     void decreaseItemCount(String id, int count);
 
+    /**
+     * Sets the exact count of an item in the economy.
+     *
+     * @param id The item identifier
+     * @param count The new count value
+     */
     void setItemCount(String id, int count);
 
+    /**
+     * Creates a new DynamicEconomy instance with specified controllers.
+     *
+     * @param itemController The item controller implementation
+     * @param priceController The price controller implementation
+     * @return A new DynamicEconomy instance
+     * @throws NullPointerException if any controller is null
+     */
     static DynamicEconomy create(@NotNull ItemController itemController, @NotNull PriceController priceController) {
         return new DynamicEconomyImpl(itemController, priceController);
     }
 
+    /**
+     * Returns a builder for creating DynamicEconomy instances.
+     *
+     * @return A new builder instance
+     */
     static DynamicEconomyImpl.DynamicEconomyImplBuilder builder() {
         return DynamicEconomyImpl.builder();
     }
--- src/main/java/com/mefrreex/dynamiceconomy/api/model/ItemData.java
+++ src/main/java/com/mefrreex/dynamiceconomy/api/model/ItemData.java
@@ -1,5 +1,16 @@
 package com.mefrreex.dynamiceconomy.api.model;
 
+/**
+ * Represents an item data including its base price and initial amount.
+ *
+ * @param basePrice The base price of the item
+ * @param initialAmount The initial quantity of the item
+ */
 public record ItemData(double basePrice, int initialAmount) {
+
+    /**
+     * An empty item data instance with zero values.
+     * Can be used as a default or placeholder value.
+     */
     public static final ItemData EMPTY = new ItemData(0, 0);
 }
